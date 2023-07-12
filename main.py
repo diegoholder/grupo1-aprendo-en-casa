dff6198ad80d0fe0d617bc726b553ac4a132bb8f
Diego Holder <diegoholderj@gmail.com>
2023-07-12 00:14:43 -0500

    Se añadió comentarios al código.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-os.system("cls")
+os.system("cls") #Sirve para limpiar la pantalla
 
+#Todas las variables que debe tener el material
 titulo = ""
 curso = ""
 autor = ""
@@ -9,14 +10,18 @@ modalidad = ""
 nivel = ""
 grado = ""
 idioma = ""
+
+#Tuplas para las variables que van a tener valores predefinidos
 MODALIDADES = ("Básica regular", "Básica alternativa", "Básica especial")
 NIVELES = ("Inicial", "Primaria", "Secundaria", "Otros")
 GRADOS = ("1ro", "2do", "3ro", "4to", "5to", "6to", "Otros")
 IDIOMAS = ("Castellano", "Quechua", "Aymara", "Inglés", "Otros")
 CURSOS = ("Comunicación", "Matemáticas", "Ciencias", "Inglés")
 
+#Lista que va a contener todos los materiales creados (los materiales son diccionarios)
 lista_materiales = []
 
+#Definiendo las opciones que va a tener el menú principal
 opciones_de_menu = {
     1: 'Ingreso de materiales nuevos',
     2: 'Visualización de materiales educativos',
@@ -24,6 +29,7 @@ opciones_de_menu = {
     4: 'Salir del programa',
 }
 
+#Definiendo la función para imprimir el menú y todas sus opciones definidas en el diccionario anterior^
 def imprimir_menu():
     print('==============================================')
     print('\tAPRENDO EN CASA')
@@ -31,6 +37,7 @@ def imprimir_menu():
     for key in opciones_de_menu.keys():
         print (key, '--', opciones_de_menu[key] )
 
+#Definiendo la función para la opción 1 (Ingreso de materiales)
 def opcion_1():
      os.system("cls")
      print('==============================================')
@@ -85,6 +92,7 @@ def opcion_1():
      os.system("cls")
      print("Se añadió el material con éxito.")
 
+#Definiendo la función para la opción 2 (Visualización de materiales)
 def opcion_2():
      os.system("cls")
      print('==============================================')
@@ -92,12 +100,14 @@ def opcion_2():
      print('==============================================')
      print(lista_materiales)
 
+#Definiendo la función para la opción 3 (Edición de materiales)
 def opcion_3():
      os.system("cls")
      print('==============================================')
      print('\tEDICIÓN DE MATERIALES')
      print('==============================================')
 
+#Ejecución del programa, se llama a las opciones definidas arriba de acuerdo a la selección del usuario
 if __name__=='__main__':
     while(True):
         imprimir_menu()
